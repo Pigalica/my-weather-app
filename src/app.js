0895ec742ceb1d4b37eec0a5ca66a3dde4a086da
Dasha <ivanko.dasha@gmail.com>
2022-10-29 21:11:41 +0300

    Add display of day of week and time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,32 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let time = `${hours}:${minutes}`;
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[date.getDay()];
+
+  return `${day} ${time}`;
+}
+
 function displayWeatherConditions(response) {
   let temperature = Math.round(response.data.temperature.current);
   let wind = Math.round(response.data.wind.speed);
@@ -11,6 +40,7 @@ function displayWeatherConditions(response) {
     "#displayedCityAndCountry"
   );
   let iconElement = document.querySelector("#icon");
+  let dateElement = document.querySelector("#date-and-time");
   temperatureElement.innerHTML = temperature;
   windElement.innerHTML = wind;
   displayedCityAndCountry.innerHTML = `${city}, ${country}`;
@@ -20,6 +50,7 @@ function displayWeatherConditions(response) {
     `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
   );
   iconElement.setAttribute("alt", response.data.condition.description);
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
   console.log(response.data);
 }
 
